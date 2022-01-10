Adicitus/ps-build-module

commit 4f0ed57d8fd8ae24459c2ff8ac1d8cb08db10612

Fixed unapproved verb and incremented version.

build.settings/manifest/moduleversion.ps1
@@ -1,5 +1,1 @@
-$buildnumFile = "$PSScriptRoot\.meta\buildnum"
-
-$buildnum = Get-Content $buildnumFile
-
-"1.0.0.{0}" -f $buildnum
+"1.0.0.2"

Build.ps1
@@ -5,7 +5,7 @@ param(
     [string]$OutRoot = "$ProjectRoot\out"
 )
 
-. "$PSScriptRoot\source\Build-ModuleProject.ps1"
+. "$PSScriptRoot\source\Export-ModuleProject.ps1"
 
 $args = @{
     ProjectRoot = $ProjectRoot
@@ -15,4 +15,4 @@ if ($OutRoot) {
     $args.OutRoot = $OutRoot
 }
 
-Build-ModuleProject @args
+Export-ModuleProject @args

build.settings/manifest/.meta/buildnum
@@ -1,1 +1,0 @@
-0

build.settings/manifest/aliasestoexport.ps1
@@ -1,0 +1,3 @@
+@(
+    "Build-ModuleProject"
+)

source/.bootstrap.ps1
@@ -1,0 +1,1 @@
+New-Alias -Name Build-ModuleProject -Value Export-ModuleProject

source/Export-ModuleProject.ps1
@@ -20,12 +20,12 @@ The .ps1 files i a directory will be processed in lexicographical order. Subdire
 will also be processed in this order.
 #>
 
-function Build-ModuleProject {
+function Export-ModuleProject {
     [CmdletBinding()]
     param(
         [ValidateNotNullOrEmpty()]
-        [Parameter(Mandatory=$true, Position=1)]
-        [string]$ProjectRoot,
+        [Parameter(Mandatory=$false, Position=1)]
+        [string]$ProjectRoot=".",
         [ValidateNotNullOrEmpty()]
         [Parameter(Mandatory=$false, Position=2)]
         [string]$OutRoot = "$ProjectRoot\out"

commit 7ddea973555e3a8271dd71727c01bfa23309a372

Fixed error introduced in 4f0ed57d8fd8

build.settings/manifest/functionstoexport.ps1
@@ -1,4 +1,4 @@
 @(
-    "Build-ModuleProject"
+    "Export-ModuleProject"
     "Initialize-ModuleProjectDirectory"
 )

build.settings/manifest/moduleversion.ps1
@@ -1,1 +1,1 @@
-"1.0.0.3"
+"1.0.0.4"